680c1451e2a9d517aacdd0462ceda70c4d98f6c9
Oleksandr Tsurkan <aleks6699@gmail.com>
2025-03-08 07:56:56 +0200

    fix: fonts

--- testing-project/src/app/layout.tsx
+++ testing-project/src/app/layout.tsx
@@ -4,38 +4,35 @@ import "./globals.css";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 
-export const poppins = Poppins({
+// Инициализация шрифтов
+const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
   display: "swap",
 });
 
-export const roboto = Roboto({
+const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
-  variable: "--font-roboto",
   display: "swap",
 });
 
-export const inter = Inter({
+const inter = Inter({
   subsets: ["latin"],
   weight: ["500", "800"],
-  variable: "--font-inter",
   display: "swap",
 });
 
-export const manrope = Manrope({
+const manrope = Manrope({
   subsets: ["latin"],
   weight: ["500"],
-  variable: "--font-manrope",
   display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "My project",
   description: "Testing project",
-  authors: [{ name: "Oleksandr", url: "hhttps://github.com/aleks6699" }],
+  authors: [{ name: "Oleksandr", url: "https://github.com/aleks6699" }],
   keywords: "Next.js, React, TypeScript, project",
 };
 
@@ -46,9 +43,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} ${roboto.variable} ${inter.variable} ${manrope.variable}`}
-      >
+      <body className={`${poppins.className} ${roboto.className} ${inter.className} ${manrope.className}`}>
         <Header />
         {children}
         <Footer />
